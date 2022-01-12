a4f5ec6f69ed45761079a48c8a428d620fe8f7c7
fmsrodrigues <filipe_msr@hotmail.com>
2022-01-12 12:08:55 -0300

    finished project

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const packageJSON = require('../package.json');
+
+const commonConfig = require('./webpack.common');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageJSON.dependencies
+    }),
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
